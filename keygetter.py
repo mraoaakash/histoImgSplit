8c1f250c8e9ade5b49f74ef310d0414953db0f11
Aakash Rao <mrao.aakash@gmail.com>
2022-10-31 14:45:51 +0530

    Sanity check

--- keygetter.py
+++ keygetter.py
@@ -8,12 +8,13 @@ import fnmatch # for matching file names
 exclude = ['clusters', 'ex_datasets', 'grand-challenge-data', 'segments', 'tSNE', 'benchmark', 'NewDatasetHnE', 'histoimgsplit']
 def imgRet(key='hne'):
     for subdir, dirs, files in os.walk("/storage/tnbc"):
+        print(dirs)
         dirs[:] = [d for d in dirs if d not in exclude]
         for file in files:
             if fnmatch.fnmatch(file, '*.tif'):
                 if key in file.lower() and (("cropped" in file.lower())==False):
                     prpath=str(os.path.join(subdir, file))
-                    print(file)
+                    # print(file)
                     # if os.path.isfile(os.path.join("/storage/tnbc/NewDatasetHnE/work_copy", file)) == False:
                     #     print(f"")
                         # shutil.copy(prpath, "/storage/tnbc/NewDatasetHnE/work_copy")
