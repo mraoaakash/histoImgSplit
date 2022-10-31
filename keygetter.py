5c1b6d4dbb3f0a1ab1f42dc2d1fffcb5142a57fa
Aakash Rao <mrao.aakash@gmail.com>
2022-10-31 14:08:55 +0530

    Added the base for master json creator

---
+++ keygetter.py
@@ -0,0 +1,19 @@
+import shutil # for copying files
+import os # for checking if file exists
+import numpy as np # for image processing
+import fnmatch # for matching file names
+exclude = ['clusters', 'ex_datasets', 'grand-challenge-data', 'segments', 'tSNE', 'benchmark', 'NewDatasetHnE']
+def imgRet(key='hne'):
+    for subdir, dirs, files in os.walk("/storage/tnbc"):
+        dirs[:] = [d for d in dirs if d not in exclude]
+        for file in files:
+            if fnmatch.fnmatch(file, '*.tif'):
+                if key in file.lower() and (("cropped" in file.lower())==False):
+                    prpath=str(os.path.join(subdir, file))
+                    print(file)
+                    shutil.copy(prpath, "/storage/tnbc/segments/224:224/send_og")
+					#call yout preferred function here
+					#can be	randcrop or imgcrop or any
+					#custom function
+
+imgRet('hne')
