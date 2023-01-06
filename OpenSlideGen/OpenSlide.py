bf51ba36a52e92f2cd6ef5dc4caa2d5ecc6cefc4
Aakash Rao <mrao.aakash@gmail.com>
2023-01-06 13:18:13 +0530

    Added multiprocessing

--- OpenSlideGen/OpenSlide.py
+++ OpenSlideGen/OpenSlide.py
@@ -5,15 +5,16 @@ import os
 import json
 from PIL import Image 
 import PIL 
+from multiprocessing import Pool, Process
+import multiprocessing as mp
+import concurrent.futures
 
 
-image = openslide.OpenSlide("/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/histoImgSplit/testImage/20190610_541_528-18_1412-18-A_Biopsy_TNBC_HnE_40X.tif")
-# image = openslide.OpenSlide("/Users/mraoaakash/Documents/research/research-tnbc/histoImgSplit/testImage/20190610_541_528-18_1412-18-A_Biopsy_TNBC_HnE_40X.tif")
-# print(image.dimensions)
-tile_size =[256,512,1024]
-for p in range(len(tile_size)):
-    x = tile_size[p]
-    dzoomImg = DeepZoomGenerator(image, tile_size=x, overlap=1, limit_bounds=True)
+def getTiles(tile_size):
+    image = openslide.OpenSlide("/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/histoImgSplit/testImage/20190610_541_528-18_1412-18-A_Biopsy_TNBC_HnE_40tile_size.tif")
+    # image = openslide.OpenSlide("/Users/mraoaakash/Documents/research/research-tnbc/histoImgSplit/testImage/20190610_541_528-18_1412-18-A_Biopsy_TNBC_HnE_40X.tif")
+    # print(image.dimensions)
+    dzoomImg = DeepZoomGenerator(image, tile_size=tile_size, overlap=1, limit_bounds=True)
     # print(dzoomImg.level_count)
     # print(dzoomImg.level_tiles)
     print(dzoomImg.level_dimensions)
@@ -25,10 +26,21 @@ for p in range(len(tile_size)):
         for j in range(0,leveltiles[0]):
             for k in range(0,leveltiles[1]):
                 deepzwsi = dzoomImg.get_tile(i, address = (j, k))
-                if not os.path.isdir(f"/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/histoImgSplit/OpenSlideGen/{x}/level{str(i)}"):
-                        os.makedirs(f"/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/histoImgSplit/OpenSlideGen/{x}/level{str(i)}")
-                im1 = deepzwsi.save(f"/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/histoImgSplit/OpenSlideGen/{x}/level{str(i)}/downsampWSI_level{str(i)}_{str(j)}_{str(k)}.jpg")
-                # if not os.path.isdir(f"/Users/mraoaakash/Documents/research/research-tnbc/histoImgSplit/OpenSlideGen/{x}/level{str(i)}"):
-                #         os.makedirs(f"/Users/mraoaakash/Documents/research/research-tnbc/histoImgSplit/OpenSlideGen/{x}/level{str(i)}")
-                # im1 = deepzwsi.save(f"/Users/mraoaakash/Documents/research/research-tnbc/histoImgSplit/OpenSlideGen/{x}/level{str(i)}/downsampWSI_level{str(i)}_{str(j)}_{str(k)}.jpg")
+                if not os.path.isdir(f"/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/histoImgSplit/OpenSlideGen/{tile_size}/level{str(i)}"):
+                        os.makedirs(f"/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/histoImgSplit/OpenSlideGen/{tile_size}/level{str(i)}")
+                im1 = deepzwsi.save(f"/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/histoImgSplit/OpenSlideGen/{tile_size}/level{str(i)}/{str(i)}_{str(j)}_{str(k)}.jpg")
+                # if not os.path.isdir(f"/Users/mraoaakash/Documents/research/research-tnbc/histoImgSplit/OpenSlideGen/{tile_size}/level{str(i)}"):
+                #         os.makedirs(f"/Users/mraoaakash/Documents/research/research-tnbc/histoImgSplit/OpenSlideGen/{tile_size}/level{str(i)}")
+                # im1 = deepzwsi.save(f"/Users/mraoaakash/Documents/research/research-tnbc/histoImgSplit/OpenSlideGen/{tile_size}/level{str(i)}/{str(i)}_{str(j)}_{str(k)}.jpg")
 
+
+if __name__ == '__main__':
+    tile_size = [256, 512, 1024]
+    pool = Pool(mp.cpu_count())
+    pool.map(getTiles, tile_size)
+    pool.close()
+    pool.join()
+
+
+
+    
